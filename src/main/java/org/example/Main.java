e713f236010502d3330d3dbea8e42100de02f65a
Mahmut Aktaş <mahmutaktas.m@gmail.com>
2024-08-09 00:49:53 +0300

    S15D4 completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,38 @@
 package org.example;
 
 
+import java.util.LinkedList;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
     }
+
+    public static Boolean checkForPalindrome(String word) {
+        String reversed = "";
+        String cleanedWord = word.toLowerCase(Locale.ENGLISH).replaceAll("[\\s.,!?\"']", "");
+        for (int i = cleanedWord.length() - 1; i >= 0; i--) {
+            reversed += cleanedWord.charAt(i);
+        }
+        return cleanedWord.equals(reversed);
+    }
+
+
+    public static String convertDecimalToBinary(int decimal) {
+        LinkedList<Integer> numbers = new LinkedList<>();
+        while (decimal != 0) {
+            numbers.addFirst(decimal % 2);
+            decimal /= 2;
+
+        }
+        StringBuilder result = new StringBuilder();
+        for (Integer i : numbers) {
+            result.append(i);
+        }
+        return result.toString();
+    }
+
+
 
 }
